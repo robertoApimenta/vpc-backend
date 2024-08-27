1a706d05d874bfba0ab08ccf81f544c326a15bf8
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2024-08-27 18:21:37 -0300

    correcao novo banco

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,8 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('postgres', 'postgres', 'Vaipraconta!#', {
-    host: 'vaipraconta.cfm4ams0261o.us-east-1.rds.amazonaws.com',
+const sequelize = new Sequelize('postgres', 'postgres', 'l1IYOR9AolHJ1o3pSc6K', {
+    host: 'vaiparacontaprod01.cfm4ams0261o.us-east-1.rds.amazonaws.com',
     dialect: 'postgres',
+    dialectOptions: { //< Add this
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default sequelize;
