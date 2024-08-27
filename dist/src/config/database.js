a4f1f068d8689a7b1ade276d32e4c11680d06150
Roberto Ayres Pimenta <87720894+robertoApimenta@users.noreply.github.com>
2024-08-27 18:27:41 -0300

    New Build

--- dist/src/config/database.js
+++ dist/src/config/database.js
@@ -2,7 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize('postgres', 'postgres', 'l1IYOR9AolHJ1o3pSc6K', {
-    host: 'vaiparacontaprod01.cfm4ams0261o.us-east-1.rds.amazonaws.com/',
+    host: 'vaiparacontaprod01.cfm4ams0261o.us-east-1.rds.amazonaws.com',
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 exports.default = sequelize;
